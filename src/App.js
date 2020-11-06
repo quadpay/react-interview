32a269f062f9e09ef6858b25df1907acb7f64818
cameron-quad <cameron.moss@quadpay.com>
2020-11-06 16:42:47 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
+import { useEffect } from 'react';
 
 function App() {
+
+  /**
+    * This function calculates the installments for a purchase plan.
+    * Assume there are always four installments.
+    * The function need not return anything but should be used to display
+    * the four installments back to the user after the user inputs a valid
+    * dollar amount and clicks submit.
+    * @param {number} amount the amount the customer intends to purchase
+  */
+  const createPaymentPlan = amount => {
+    console.log("Creating payment plan with amount: $", amount);
+  }
+
+  useEffect(() => {
+    // for testing purposes, a purchase amount of $100 should calculate four
+    // installments of $25
+    createPaymentPlan(100);
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form>
+          <label>How much do you plan to spend?</label>
+          <input type="text" id="amount" />
+          <input type="submit" value="Submit" />
+        </form>
       </header>
     </div>
   );
